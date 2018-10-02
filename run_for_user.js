d19e05c9ca353007dac73d1aa4ec92aaa8730bdc
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-10-02 19:13:29 -0300

    Sending raw tx

--- run_for_user.js
+++ run_for_user.js
@@ -3,6 +3,7 @@ const secrets = require('./secrets');
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const SmartCarInsuranceContract = require('./ethereum/build/SmartCarInsuranceContract.json');
 const configs = require('./configs');
+const Tx = require('ethereumjs-tx')
 
 const user_idx = parseInt(process.argv[2]);
 let nTransactions = 0;
@@ -13,31 +14,82 @@ const provider = new HDWalletProvider(
     user_idx
 );
 
-const myWeb3 = new Web3(provider);
-const smartCarInsuranceContract = new myWeb3.eth.Contract(JSON.parse(SmartCarInsuranceContract.interface), configs.contractAddress);
+// console.log(provider);
+// console.log(provider.hdwallet._hdkey._privateKey);
+// console.log(provider.hdwallet._hdkey._publicKey);
+
+const privKeyBuffer = provider.wallet._privKey;
+const pubKeyBuffer = provider.wallet._pubKey;
+const accountAddress = provider.address;
+
+// console.log(accountAddress);
+// console.log(pubKeyBuffer.toString("hex"));
+
+const web3 = new Web3(provider);
+const smartCarInsuranceContract = new web3.eth.Contract(JSON.parse(SmartCarInsuranceContract.interface), configs.contractAddress);
+
+let currentLat = configs.minInitialLat + (configs.maxInitialLat-configs.minInitialLat)*Math.random();
+let currentLong = configs.minInitialLong + (configs.maxInitialLong-configs.minInitialLong)*Math.random();
+
+
+
+function sendSigned(txData,  cb) {
+    const transaction = new Tx(txData);
+    transaction.sign(privKeyBuffer);
+    const serializedTx = transaction.serialize().toString('hex');
+    web3.eth.sendSignedTransaction('0x' + serializedTx, cb);
+}
 
 setTimeout(() => {
     setInterval(async () => {
-        const accounts = await myWeb3.eth.getAccounts();
         let thisTransaction = nTransactions++;
         const currentUnixTimestamp = Math.floor(Date.now()/1000);
-        console.log(`Sending transaction ${thisTransaction} for user ${user_idx} (${accounts[0]}) at ${currentUnixTimestamp}`);
-        smartCarInsuranceContract.methods.pushGpsData(currentUnixTimestamp, "Abc").send(
-            {
-                gas: '1000000',
-                from: accounts[0]
+        console.log(`Sending transaction ${thisTransaction} for user ${user_idx} (${accountAddress}) at ${currentUnixTimestamp}`);
+        latLongData = {
+            lat: currentLat,
+            long: currentLong
+        }
+        currentLat += (Math.random() > 0.5 ? 1 : -1)*Math.random()*configs.maxCoordinateDeltaBetweenCalls;
+        currentLong += (Math.random() > 0.5 ? 1 : -1)*Math.random()*configs.maxCoordinateDeltaBetweenCalls;
+
+        const data = smartCarInsuranceContract.methods.pushGpsData(currentUnixTimestamp, JSON.stringify(latLongData)).encodeABI();
+        // console.log(data);
+
+        // TODO: Encriptar latLongData
+        // get the number of transactions sent so far so we can create a fresh nonce
+        web3.eth.getTransactionCount(accountAddress).then(txCount => {
+            // console.log(`txCount for ${accountAddress} is ${txCount}`);
+            const txData = {
+                nonce: web3.utils.toHex(txCount),
+                gasLimit: web3.utils.toHex(250000),
+                gasPrice: web3.utils.toHex(1e9), // 1 Gwei
+                to: configs.contractAddress,
+                from: accountAddress,
+                data: data
             }
-        )
-            .then((transactionData) => {
-                console.log("-----------------------------------------------------------------------");
-                console.log(`Transaction ${thisTransaction} of user ${user_idx} finished successfuly:`);
-                console.log(transactionData);
-                
-            })
-            .catch((err) => {
-                console.log("-----------------------------------------------------------------------");
-                console.log(`Transaction ${thisTransaction} of user ${user_idx} finished with ERROR:`);
-                console.log(err);
+            // fire away!
+            sendSigned(txData, function(err, result) {
+                if (err) return console.log('error', err);
+                console.log('sent', result);
             });
+        })
+
+        // smartCarInsuranceContract.methods.pushGpsData(currentUnixTimestamp, JSON.stringify(latLongData)).send(
+        //     {
+        //         gas: '1000000',
+        //         from: accounts[0]
+        //     }
+        // )
+        //     .then((transactionData) => {
+        //         console.log("-----------------------------------------------------------------------");
+        //         console.log(`Transaction ${thisTransaction} of user ${user_idx} finished successfuly:`);
+        //         console.log(transactionData);
+                
+        //     })
+        //     .catch((err) => {
+        //         console.log("-----------------------------------------------------------------------");
+        //         console.log(`Transaction ${thisTransaction} of user ${user_idx} finished with ERROR:`);
+        //         console.log(err);
+        //     });
     }, configs.sendLocationPeriodInMiliseconds);
 }, Math.random() * configs.sendLocationPeriodInMiliseconds);
